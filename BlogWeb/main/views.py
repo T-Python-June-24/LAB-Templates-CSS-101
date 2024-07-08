3ed0c3090328955117147c72ee0ff73c9ebaf675
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-07-08 10:55:41 +0300

    Creaet django project and app

---
+++ BlogWeb/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+
+def home_view (request: HttpRequest):
+    pass
+def aboutUs_view (request: HttpRequest):
+    pass
+def ContactUs_view (request: HttpRequest):
+    pass
+def Career_view (request: HttpRequest):
+    pass
