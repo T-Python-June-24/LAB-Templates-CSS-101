571b1a96c5a0a535879b73b67d5b6cbdb5a35051
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-07-09 08:32:08 +0300

    Completed LAB-Templates-CSS-101

---
+++ WebpageDesignPractice/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+
+# Create your views here.
+def index_view(request: HttpRequest):
+    return render(request, 'main/index.html')
+
+
+def rental(request: HttpRequest):
+    return render(request, 'main/rental.html')
